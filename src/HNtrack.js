372aad5fb7193149eb369dc0c9b7a1ab19eb4c5c
sunlandong <sunlandong@9086.cn>
2018-01-15 18:44:17 +0800

    埋点

---
+++ src/HNtrack.js
@@ -0,0 +1,41 @@
+
+
+export default class HNtrack {
+
+    constructor(config){
+        this.init(config)
+    }
+
+    init(config){
+        this.config = Object.assign({},this.config,config)
+        if(this.config.isAutoTrack){
+            this.autoTrack()
+        }
+    }
+
+    autoTrack(){
+        let aList = document.getElementsByTagName("a")
+        aList.addEventListener("click",()=>{
+
+        })
+        let butonList = document.getElementsByTagName("button")
+        butonList.addEventListener("click",()=>{
+            
+        })
+        let inputList = document.getElementsByTagName("input")
+        inputList.addEventListener("click",()=>{
+            
+        })
+    }
+
+    config = {
+        max_referrer_string_length:200,//referer字符串截取最大长度
+        callback_timeout: 1000,
+        is_track_device_id: false,//埋点的设备id
+        use_app_track: false,//后期可能使用，在app内的时候，所有埋点数据，都推送到app，由app推送到服务端
+        server_url:'',
+        isAutoTrack:false
+    }
+}
+
+window.HNtrack = HNtrack
---
+++ version.js
@@ -0,0 +1,1 @@
+export const haina_data_tracking_version = '0.0.1'
