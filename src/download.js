0fc3411580ca8790b9dfab7e3f240bfbc2663bf5
sunlandong <sunlandong@9086.cn>
2018-01-22 14:42:16 +0800

    download

--- src/download.js
+++ src/download.js
@@ -1,79 +1,83 @@
 /**
  * Created by Administrator on 2017/10/24 0024.
  */
-;(function () {
+; (function () {
     var ua = window.navigator.userAgent.toLowerCase();
 
     function _IsInApp() {
         return ua.indexOf('hayner') > 1;
     }
-    $.fn.downLoadApp = function (options) {
-
-        options = $.extend({
-            boxHeight: '1.2rem',
-            downImg: "downImg",
-            colsed: "colsed",
-            downLoadImgUrl: ''
-        }, options);
-
-        return this.each(function (i) {
-            if ($(this).find('.' + options.downImg).length <= 0) {
-                var downImg = $("<div />", {
-                    "class": options.downImg,
-                    "css": {
-                        "width": '100%',
-                        "height": options.boxHeight,
-                        'background': 'url(' + options.downLoadImgUrl + ')',
-                        'background-size': '100%'
-                    }
-                });
-                $(this).append(downImg);
-                $('.' + options.downImg).click(function () {
-                    window.location = 'http://a.app.qq.com/o/simple.jsp?pkgname=com.sz.nniu';
-                })
-            }
-
-            if (!$(this).find('.' + options.colsed).length <= 0) {
-                var colsed = $("<div />", {
-                    "class": options.colsed,
-                    "css": {
-                        "width": '0.5rem',
-                        "height": '0.5rem',
-                        "background": 'red',
-                        "position": 'absolute',
-                        "right": '0.5rem',
-                        "top": '-0.5rem'
-                    }
-                });
-                $(this).append(colsed);
-                $('.' + options.colsed).click(function () {
-                    $(this).parent().hide();
-                })
-            }
-
-            $(this).css({
+    function generaterFooter() {
+        let $footer = $('<div />', {
+            "id":"downloadApp",
+            "css": {
                 'position': 'fixed',
                 'left': 0,
+                'right':0,
                 'bottom': 0,
                 'width': '100%',
-                'height': options.boxHeight,
+                'height': "1.2rem",
                 'z-index': 99
-            });
-
-            if (_IsInApp()) {
-                $(this).hide();
             }
-
+        })
+        let downLoadImgUrl = 'https://m2.0606.com.cn/assets/images/zx_load.png'
+        let $downImg = $("<div />", {
+            "css": {
+                "width": '100%',
+                "height": "1.2rem",
+                'background': 'url(' + downLoadImgUrl + ')',
+                'background-size': '100%'
+            }
         });
-
-    };
-    
-    if (!_IsInApp()) {
-        // $("body").css("margin-bottom","1.2rem")
-        $('html').append('<div id="downloadApp"></div>');
-        $('#downloadApp').downLoadApp({
-            downLoadImgUrl: 'https://m2.0606.com.cn' + '/assets/images/zx_load.png'
+        let $a = $('<a />', {
+            "class": "download",
+            "css": {
+                "position": "absolute",
+                "top":0,
+                "left":0,
+                "width": "100%",
+                "height": "100%",
+                "color": "#fff",
+                "text-decoratio": "none"
+            }
+        })
+        $footer.append($downImg)
+        $footer.append($a)
+        $footer.on("click", ".download", function () {
+            window.location = 'http://a.app.qq.com/o/simple.jsp?pkgname=com.sz.nniu';
+        })
+        let $colsed = $("<span />", {
+            "class": "downclose",
+            "css": {
+                "display":"flex",
+                "justify-content":"center",
+                "align-items":"center",
+                "width": '0.4rem',
+                "height": '0.4rem',
+                "background": '#777',
+                "position": 'absolute',
+                "right": "0.1rem",
+                "top": "0.1rem",
+                "border-radius":"50%",
+                "font-size":"0.3rem",
+                "color":"#aaa"
+            }
         });
+        $colsed.append("<strong style='background: #777;'>x</strong>")
+        $footer.append($colsed);
+        $footer.on("click", ".downclose", function () {
+            $(this).parent().hide();
+        })
+        $('html').append($footer);
+    }
+    function getQuertString(key) {
+        let reg = new RegExp("(^|&)" + key + "=([^&]*)(&|$)");
+        let r = window.location.search.substr(1).match(reg);
+        if (r != null) return decodeURI(r[2]);
+        return null;
+    }
+    if (!_IsInApp() && getQuertString("sharefrom") === 'hayner') {
+        generaterFooter()
     }
 
 
