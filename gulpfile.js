feebf2a9f150f558a066a29c691d5f0f47a7b341
sunlandong <sunlandong@9086.cn>
2018-01-25 11:21:08 +0800

    '脚本更新'

--- gulpfile.js
+++ gulpfile.js
@@ -16,18 +16,15 @@ gulp.task("lib", function () {
         }))
         .pipe(uglify())
         .pipe(gulp.dest('dist'))
-        .pipe(gzip({threshold: 10240,minRatio:0.8}))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('../haina-mobile/assets/js'))
 })
 
 gulp.task("lib-2.1", function () {
     gulp.src(['src/zepto.js','src/download.js'])
-        .pipe(concat(`hntrack_${version}.js`))
+        .pipe(concat(`hntrack_0.0.2.js`))
         .pipe(babel({
             presets:['es2015','stage-0']
         }))
         .pipe(uglify())
-        .pipe(gulp.dest('dist'))
-        .pipe(gzip({threshold: 10240,minRatio:0.8}))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist2.1'))
 })
