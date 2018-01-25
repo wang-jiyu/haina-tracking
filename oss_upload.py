03ed937d127529ee7f1702b3512ae5b0c3ea0f3b
sunlandong <sunlandong@9086.cn>
2018-01-25 11:21:55 +0800

    '上传oss脚本'

---
+++ oss_upload.py
@@ -0,0 +1,23 @@
+#coding:utf-8
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkcore.request import RpcRequest
+import commands
+import oss2
+import os
+
+class OSS_resouce(object):
+    def __init__(self):
+        self.auth = oss2.Auth('LTAIPbl6650CGCWd','5bozPnpcPvquE5CtUTSz09KTGMLAv7')
+
+    def bin_upload_files(self):
+        self.bucket = oss2.Bucket(self.auth, 'http://oss-cn-beijing.aliyuncs.com', 'hn-web')
+        self.file_list = os.listdir('./dist2.1')
+        for file in self.file_list:
+            if os.path.exists(file):
+            	# print 'aaa'
+                self.bucket.put_object_from_file('web/js/{remote_file}'.format(remote_file = file),'{local_name}'.format(local_name = file))
+
+        print 'update '+str(len(self.file_list))+' files'
+if __name__ == '__main__':
+    OSS_resouce().bin_upload_files()
+ 
